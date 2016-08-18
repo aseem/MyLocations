92f1d99b7b9a0328bbf6778e661e0954097372bd
Aseem Kohli <aseemko@microsoft.com>
2016-08-18 14:45:53 -0400

    P 229

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -161,6 +161,10 @@ class LocationDetailsViewController: UITableViewController {
         if indexPath.section == 0 && indexPath.row == 0 {
             self.descriptionTextView.becomeFirstResponder()
         }
+        else if indexPath.section == 1 && indexPath.row == 0 {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            pickPhoto()
+        }
     }
     
     // MARK: Helper Methods
@@ -191,5 +195,52 @@ class LocationDetailsViewController: UITableViewController {
     func formatDate(date: NSDate) -> String {
         return dateFormatter.stringFromDate(date)
     }
+}
+
+extension LocationDetailsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func pickPhoto() {
+        if true || UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            showPhotoMenu()
+        }
+        else {
+            choosePhotoFromLibrary()
+        }
+    }
     
+    func showPhotoMenu() {
+        let alertController = UIAlertController(title: nil, message: nil,preferredStyle: .ActionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        let takePhotoAction = UIAlertAction(title: "Take Photo",
+                                            style: .Default, handler: {_ in self.takePhotoWithCamera()})
+        alertController.addAction(takePhotoAction)
+        let chooseFromLibraryAction = UIAlertAction(title:
+            "Choose From Library", style: .Default, handler: {_ in self.choosePhotoFromLibrary()})
+        alertController.addAction(chooseFromLibraryAction)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func takePhotoWithCamera() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .Camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func choosePhotoFromLibrary() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .PhotoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
